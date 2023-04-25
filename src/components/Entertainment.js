f78f2cc0f56eb47ff08cab8970f664c95a12b794
nmlobos <nmlobos1999@gmail.com>
2023-04-25 16:41:12 -0600

    next commit

--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -1,15 +1,48 @@
 import React from "react"
-import './Entertainment';
+import './Entertainment.css';
 import { Container } from "react-bootstrap";
 
 
 export function Entertainment() {
     return (
         <Container className="entertainmnetbackground">
+
             <h1>Entertainment in the 1930s</h1>
+
             <body>
-                <h1 className="citynames">Television</h1>
+
+
+                <h1 className="citynames">Television and Films</h1>
+
+                <div className="tvphotos">
+                    <div className="columntv">
+                        <img width="100%" height='300px' src="https://images.mubicdn.net/images/film/919/cache-8458-1657550612/image-w1280.jpg?size=600x" />
+                    </div>
+                    <div className="columntv">
+                        <img width="100%" height="300px" src="https://images.mubicdn.net/images/film/1758/cache-32987-1562327441/image-w1280.jpg?size=600x" />
+                    </div>
+                    <div className="columntv">
+                        <img width="100%" height="300px" src="https://s3.amazonaws.com/static.rogerebert.com/uploads/review/primary_image/reviews/great-movie-latalante-1934/EB20001015REVIEWS0810150301AR.jpg" />
+                    </div>
+                    <div className="columntv">
+                        <img width="100%" height="300px" src="https://s3.amazonaws.com/static.rogerebert.com/uploads/review/primary_image/reviews/great-movie-duck-soup-1933/EB20000709REVIEWS087090301AR.jpg" />
+                    </div>
+                </div>
+
+                <div>
+                    <h3>Popular Movies and Televison of the 1930s</h3>
+
+                    <h4>Summary</h4>
+
+
+                </div>
+
+
+
+
+
             </body>
+
         </Container>
     )
 }
